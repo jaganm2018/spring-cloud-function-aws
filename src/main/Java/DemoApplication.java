124badc7261fa8144c3d8eeeb1180bdcab149f9b
jaganm2018 <84048983+jaganm2018@users.noreply.github.com>
2021-06-10 16:41:03 +0530

    Update DemoApplication.java

--- src/main/Java/DemoApplication.java
+++ src/main/Java/DemoApplication.java
@@ -1,17 +1,16 @@
 @SpringBootApplication
+@EnableConfigServer
 public class DemoApplication {
 
+	@Value("${env}")
+	private String env;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Bean
-	public Function<String, String> uppercase() {
-		return value -> value.toUpperCase();
-	}
-
-        @Bean
-	public Function<String, String> lowercase() {
-		return value -> value.toLowerCase();
+	public Function<String, String> fetchEnv() {
+		return value -> env;
 	}
 }
